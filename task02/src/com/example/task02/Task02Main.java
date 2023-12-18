e4126b0f410bd9a9c699a84ff8b94c98105eb3ec
Diana <lici4qweru090@gmail.com>
2023-12-18 05:09:09 +0500

    task 3

--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -5,7 +5,7 @@ import java.util.stream.IntStream;
 public class Task02Main {
 
     public static void main(String[] args) {
-        cycleGrayCode(2)
+        cycleGrayCode(5)
                 .limit(10)
                 .forEach(System.out::println);
     }
--- task03/src/com/example/task03/Task03Main.java
+++ task03/src/com/example/task03/Task03Main.java
@@ -1,27 +1,49 @@
 package com.example.task03;
 
 import java.util.Comparator;
+import java.util.Iterator;
+import java.util.Objects;
 import java.util.function.BiConsumer;
 import java.util.stream.Stream;
 
 public class Task03Main {
-
     public static void main(String[] args) {
-
         findMinMax(
                 Stream.of(2, 9, 5, 4, 8, 1, 3),
                 Integer::compareTo,
                 (min, max) ->
                         System.out.println("min: " + min + " / max: " + max)
         );
-
     }
 
     public static <T> void findMinMax(
-            Stream<? extends T> stream,
-            Comparator<? super T> order,
-            BiConsumer<? super T, ? super T> minMaxConsumer) {
+            Stream<? extends T> stream, //стрим, в котором нужно найти минимальный и максимальный элементы.
+            Comparator<? super T> order, //Comparator, который определяет порядок сортировки элементов в стриме
+            BiConsumer<? super T, ? super T> minMaxConsumer) { //BiConsumer, который принимает минимальный и максимальный элементы в стриме
+        if(stream == null || order == null || minMaxConsumer == null){
+            throw new NullPointerException();
+        }
 
-        // your implementation here
+        Iterator<? extends T> iterator = stream.iterator();
+
+        if(!iterator.hasNext()) {
+            minMaxConsumer.accept(null,null);
+            return;
+        }
+
+        T current = iterator.next();
+        T min = current;
+        T max = min;
+
+        while (iterator.hasNext()) {
+            current = iterator.next();
+            if(order.compare(current, min) < 0){
+                min = current;
+            }
+            if(order.compare(current, max) > 0){
+                max = current;
+            }
+        }
+        minMaxConsumer.accept(min, max);
     }
 }
